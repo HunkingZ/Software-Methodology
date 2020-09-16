ee810a109de185281c174cc3d0fd39122eff64f2
Flufflul <mufflul@gmail.com>
2020-09-15 22:54:54 -0400

    JavaDoc Update

--- Project 1 - Shopping Bag/src/ShoppingBag.java
+++ Project 1 - Shopping Bag/src/ShoppingBag.java
@@ -1,15 +1,24 @@
+//This project is made by Hanqing Zhao & Richard Xu
+
 import java.util.Arrays;
 
 public class ShoppingBag {
-    private GroceryItem[] bag;
-    private int size = 0;
-    private int capacity = 5;
+    private GroceryItem[] bag; // List of items
+    private int size = 0; // Current number of items in the bag
+    private int capacity = 5; // Maximum available number of items in the bag
 
+    /**
+     * Constructor for declaring a Shopping Bag
+     */
     public ShoppingBag() {
         bag = new GroceryItem[capacity];
     }
 
-    //Find the index of certain item in Shopping Bag
+    /**
+     * Find the index of a certain item in the Shopping Bag
+     *
+     * @param item The item you want to find
+     */
     private int find(GroceryItem item) {
         for (int i = 0; i < size; i++) {
             if (bag[i].equals(item)) {
@@ -19,24 +28,28 @@ public class ShoppingBag {
         return -1;
     }
 
+    /**
+     * Prints every current item in the Shopping Bag
+     */
     public void print() {
-        // loop every item in the bag
         for (int i = 0; i < size; i++) {
             GroceryItem itm = bag[i];
             System.out.println("•" + itm.toString());
         }
     }
 
-    //Grow the capacity of Shopping Bag
+    /**
+     * Doubles capacity of the Shopping Bag
+     */
     private void grow() {
         capacity += 5;
         bag = Arrays.copyOf(bag, capacity);
     }
 
     /**
-     * Adds an item into the shopping bag.
+     * Adds an item into the Shopping Bag
      *
-     * @param item The item you want to add to your shopping bag.
+     * @param item The item you want to add
      */
     public void add(GroceryItem item) {
         if (size >= capacity) {
@@ -46,12 +59,20 @@ public class ShoppingBag {
         size++;
     }
 
+    /**
+     * Returns the number of items in the Shopping Bag
+     *
+     * @return Size of the Shopping Bag
+     */
     public int getSize() {
         return size;
     }
 
-    //remove item in shopping bag if it exists
-
+    /**
+     * Removes a specific item from the Shopping Bag
+     *
+     * @param item The item to be removed
+     */
     public void remove(GroceryItem item) {
         if (find(item) != -1) {
             int left = find(item);
@@ -66,24 +87,36 @@ public class ShoppingBag {
         }
     }
 
-    //find the total price of items in the shopping bag
+    /**
+     * Calculates the total price of all the items in the Shopping Bag
+     *
+     * @return Calculated total
+     */
     public double salesPrice() {
         int total_sales = 0;
+
         for (int i = 0; i < size; i++) {
             total_sales += bag[i].price;
         }
+
         return total_sales;
     }
 
-    //find the total tax for the items
+    /**
+     * Calculates the total tax of all taxable items in the purchase
+     *
+     * @return Calculated total tax
+     */
     public double salesTax() {
         double tax_rate = 0.06625;
         double total_tax = 0;
+
         for (int i = 0; i < size; i++) {
             if (bag[i].taxable) {
                 total_tax += bag[i].price * tax_rate;
             }
         }
+
         return total_tax;
     }
 }
