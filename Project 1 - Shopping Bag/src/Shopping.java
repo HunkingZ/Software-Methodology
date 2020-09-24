7c08ece3c97e938a98c7916978ffc91b6d4d0281
Flufflul <mufflul@gmail.com>
2020-09-23 22:41:26 -0400

    Updates

--- Project 1 - Shopping Bag/src/Shopping.java
+++ Project 1 - Shopping Bag/src/Shopping.java
@@ -1,87 +1,224 @@
-//This project is made by Hanqing Zhao & Richard Xu
-import javax.sound.midi.SysexMessage;
-import java.util.*;
+import java.util.IllegalFormatException;
+import java.util.Scanner;
 
-public class Shopping { // test 2
+/**
+ * Shopping Class
+ *
+ * @author Hanqing Zhao, Richard Xu
+ */
+public class Shopping {
+    public ShoppingBag bag = new ShoppingBag();
+
+    /**
+     * Interface for handling I/O
+     */
     public void run() {
-        String st = null;
-        char c;
-        Scanner scan = new Scanner(System.in);
-        st = scan.nextLine();
-        while (st != null) {
-            c = st.charAt(0);
-            System.out.println(c);
-            //case of input, R == remove, P == print, A == add, C == checkingOut, Q == quit.
-            if (c == 'R') {
-                Scanner linescanner = new Scanner(st);
-                linescanner.next();
-                String name = linescanner.next();
-                double price = linescanner.nextDouble();
-                boolean taxable = linescanner.nextBoolean();
-                GroceryItem item = new GroceryItem(name, price, taxable);
-                remove(item);
-            } else if (c == 'P') {
-                display();
-            } else if (c == 'A') {
-                Scanner linescanner = new Scanner(st);
-                linescanner.next();
-                String name = linescanner.next();
-                double price = linescanner.nextDouble();
-                boolean taxable = linescanner.nextBoolean();
-                GroceryItem item = new GroceryItem(name, price, taxable);
-                add(item);
-            } else if (c == 'C') {
-                checking_out();
-            } else if (c == 'Q') {
-                break;
-            } else {
-                System.out.println("Invalid command!");
+        System.out.println("Let's start shopping!");
+
+        Scanner scan = new Scanner(System.in); // Scanner to read user input
+        String input = scan.nextLine();   // String to store user input information
+
+        // Full I/O handling input arguments in an indefinite loop ended if under the right condition
+        IO:
+        while (input != null) {
+            Scanner arguments = new Scanner(input); // Scanner to help parse the current user input
+            String command = "\0"; // In case of no command, sends to default switch case
+
+            if (arguments.hasNext()) { command = arguments.next(); }
+
+            switch (command) {
+                case "A": { // Perform add command
+                    GroceryItem item = createItem(arguments);
+                    if (item != null) { add(item); }
+                    break;
+                }
+                case "R": { // Perform remove command
+                    GroceryItem item = createItem(arguments);
+                    if (item != null) { remove(item); }
+                    break;
+                }
+                case "P": { // Perform display command
+                    display();
+                    break;
+                }
+                case "C": { // Perform check-out command
+                    checking_out();
+                    break;
+                }
+                case "Q": { // Perform quit command
+                    break IO;
+                }
+                default: {
+                    System.out.println("Invalid command!");
+                    break;
+                }
             }
-            st = scan.nextLine();
+
+            input = scan.nextLine();
         }
+
         System.out.println("Thanks for shopping with us!");
     }
 
-    public ShoppingBag shopping_bag = new ShoppingBag();
-    private int size = 0;
+    // Methods used for I/O parsing
 
-    private void add(GroceryItem item) {
-        shopping_bag.add(item);
-        size++;
-        System.out.println(item.name + " added to the bag.");
+    /**
+     * Checks to see if a string can be parsed as a numeric value
+     *
+     * @param value The string to be checked
+     * @return Whether the value may be parsed as a number
+     */
+    private boolean isNumeric(String value) {
+        try { Double.parseDouble(value); }
+        catch (NumberFormatException e) { return false; }
+
+        return true;
     }
 
-    private void remove(GroceryItem item) {
-            shopping_bag.remove(item);
-            if (shopping_bag.getSize() != size) {
-                size--;
-                System.out.println(item.name + " $" + item.price + " removed.");
-            } else {
-                System.out.println("Unable to remove, this item is not in the bag.");
+    /**
+     * Checks to see if a string contains only letters
+     *
+     * @param word The string to be checked
+     * @return Whether the word is fully alphabetic
+     */
+    private boolean isAlphabetic(String word) {
+        try {
+            for(int letter = 0; letter < word.length(); letter++) {
+                if (!Character.isLetter(word.charAt(letter))) { return false; }
             }
+        }
+        catch (IllegalFormatException e) { return false; }
+
+        return true;
     }
 
+    /**
+     * Confirms that the input has the correct format
+     *
+     * @param priceInput The input to be checked
+     * @return Whether the input is able to be parsed correctly
+     */
+    private boolean confirmPrice(String priceInput) {
+        if (priceInput == null || !isNumeric(priceInput)) {
+            System.out.println("Invalid command!");
+            return false;
+        }
+        return true;
+    }
+
+    /**
+     * Confirms that the input for taxable can be parsed properly as a boolean
+     *
+     * @param taxableInput The boolean input initialized and read as a string
+     * @return Whether the input is able to be parsed correctly
+     */
+    private boolean confirmTaxable(String taxableInput) {
+        // taxableInput must exactly be either "TRUE" or "FALSE"
+        if (taxableInput != null && (isAlphabetic(taxableInput) && (taxableInput.toUpperCase().equals("TRUE") || taxableInput.toUpperCase().equals("FALSE")))) {
+            return true;
+        }
+
+        System.out.println("Invalid command!");
+        return false;
+    }
+
+    /**
+     * Creates an item based on the user's input arguments
+     *
+     * @param arguments The user's input command and arguments
+     * @return A GroceryItem based on user input
+     */
+    private GroceryItem createItem(Scanner arguments) {
+        // Confirm item existence (anything not empty)
+        if (!arguments.hasNext()) {
+            System.out.println("Invalid command!");
+            return null;
+        }
+        String itemName = arguments.next();
+
+        // Confirm proper numeric input
+        if (!arguments.hasNext()) {
+            System.out.println("Invalid command!");
+            return null;
+        }
+        String priceInput = arguments.next();
+        if(!confirmPrice(priceInput)) { return null; }
+        double price = Double.parseDouble(priceInput);
+
+        // Confirm proper boolean input
+        if (!arguments.hasNext()) {
+            System.out.println("Invalid command!");
+            return null;
+        }
+        String taxableInput = arguments.next().toUpperCase();
+        if(!confirmTaxable(taxableInput)) { return null; }
+        boolean taxable = Boolean.parseBoolean(taxableInput);
+
+        return new GroceryItem(itemName, price, taxable);
+    }
+
+    // - - - - -
+
+    // Methods for performing identified commands
+
+    public static final int MIN_BAG_SIZE = 0;
+
+    /**
+     * Adds an item to the bag
+     *
+     * @param item The item to add
+     */
+    private void add(GroceryItem item) {
+        bag.add(item);
+        System.out.println(item.getName() + " added to the bag.");
+    }
+
+    /**
+     * Removes an item from the bag
+     *
+     * @param item The item to remove
+     */
+    private void remove(GroceryItem item) {
+        if (bag.getSize() == MIN_BAG_SIZE) {
+            System.out.println("Unable to remove, this item is not in the bag.");
+        } else {
+            bag.remove(item);
+            System.out.println(item.getName() + " $" + item.getPrice() + " removed.");
+        }
+    }
+
+    /**
+     * Lists all the items in the bag along with their prices and whether it is taxable
+     */
     private void display() {
-        if (size == 0) {
+        if (bag.getSize() == MIN_BAG_SIZE) {
             System.out.println("The bag is empty!");
         } else {
-            System.out.println("**You have " + size + " item(s) in the bag.");
-
-            shopping_bag.print();
-
+            System.out.println("**You have " + bag.getSize() + " item(s) in the bag.");
+            bag.print();
             System.out.println("**End of list");
         }
     }
 
+    /**
+     * Calculates total raw price, total of all taxable items, and accumulated total costs in the bag and empties the bag
+     */
     private void checking_out() {
-        System.out.println("**Checking out " + shopping_bag.getSize() + " items.");
+        if (bag.getSize() == MIN_BAG_SIZE) {
+            System.out.println("Unable to check out, the bag is empty!");
+        } else {
+            System.out.println("**Checking out " + bag.getSize() + " items.");
+            bag.print();
 
-        shopping_bag.print();
-        System.out.println("*Sales total : $" + shopping_bag.salesPrice());
-        System.out.println("*Sales tax : $" + shopping_bag.salesTax());
-        System.out.println("*Total amount paid : $" + (shopping_bag.salesPrice() + shopping_bag.salesTax()));
+            String formatted_sales_price = String.format("%.2f", bag.salesPrice());
+            String formatted_sales_tax = String.format("%.2f", bag.salesTax());
+            String formatted_sales_total = String.format("%.2f", (bag.salesPrice() + bag.salesTax()));
 
-        shopping_bag = new ShoppingBag();
-        size = 0;
+            System.out.println("*Sales total : $" + formatted_sales_price);
+            System.out.println("*Sales tax : $" + formatted_sales_tax);
+            System.out.println("*Total amount paid : $" + formatted_sales_total);
+
+            bag = new ShoppingBag();
+        }
     }
 }
