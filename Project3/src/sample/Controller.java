dbcd7e4bb8e0fbac2f4b1949df59aafb3cf4c154
Hunking <zivhunking@gmail.com>
2020-10-15 18:10:44 -0700

    controller demo

--- Project3/src/sample/Controller.java
+++ Project3/src/sample/Controller.java
@@ -1,5 +1,37 @@
 package sample;
 
-public class Controller {
+import javafx.fxml.FXML;
 
+import java.awt.*;
+import java.awt.event.ActionEvent;
+
+public class Controller {
+    @FXML
+    private TextField oca_fName, oca_lName, oca_balance, oca_month, oca_day, oca_year,
+                        dw_fName, dw_lName, dw_amount;
+
+    @FXML
+    void open(ActionEvent event) {
+
+    }
+
+    @FXML
+    void close(ActionEvent event) {
+
+    }
+
+    @FXML
+    void clear(ActionEvent event) {
+
+    }
+
+    @FXML
+    void deposit(ActionEvent event) {
+
+    }
+
+    @FXML
+    void withdraw(ActionEvent event) {
+
+    }
 }
