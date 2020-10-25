d1bf66daa4b0fa63521a2828284e64ea3d02f74f
Hunking <zivhunking@gmail.com>
2020-10-24 22:59:13 -0700

    Deleted Transcation Manager and change the name of main to Main

--- Project3/src/sample/Main.java
+++ Project3/src/sample/Main.java
@@ -1,12 +1,11 @@
 package sample;
-//package Project2-TranscationSystem;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
-public class RunProject3 extends Application {
+public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
