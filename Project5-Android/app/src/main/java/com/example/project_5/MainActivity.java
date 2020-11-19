c6804cb666ab909cbc319fff90f02c5613ab8bad
Hunking <zivhunking@gmail.com>
2020-11-19 15:05:50 -0800

    Added Project 5 - 2

---
+++ Project5-Android/app/src/main/java/com/example/project_5/MainActivity.java
@@ -0,0 +1,18 @@
+package com.example.project_5;
+
+import android.widget.Button;
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Button b1;
+    
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        
+        
+    }
+}
